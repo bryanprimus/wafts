31392e597093c9726e641abd124592f86b8e34f1
bryanprimus <bryantobing0@gmail.com>
2026-04-18 15:53:50 +0800

    feat: integrate oxfmt for code formatting, update linting configuration, and run format

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -1,21 +1,19 @@
-import { defineConfig } from "oxlint";
+import { defineConfig } from 'oxlint'
 
 export default defineConfig({
   categories: {
-    correctness: "warn",
+    correctness: 'warn',
   },
   plugins: [
     // default enabled built-in plugins https://oxc.rs/docs/guide/usage/linter/plugins.html#supported-plugins
-    "eslint",
-    "typescript",
-    "oxc",
-    "unicorn",
+    'eslint',
+    'typescript',
+    'oxc',
+    'unicorn',
 
     // default disabled built-in plugins
-    "react",
-    "react-perf"
+    'react',
+    'react-perf',
   ],
-  ignorePatterns: [
-    "routeTree.gen.ts"
-  ],
-});
+  ignorePatterns: ['routeTree.gen.ts'],
+})
